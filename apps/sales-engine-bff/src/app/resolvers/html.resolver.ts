60fbba4ed8c353d3dd766d4b920ecac8a6521b36
eduardoperezlutin <eduardoperezlutin@gmail.com>
2020-05-24 00:50:44 -0600

    Add resolvers structure && html resolver

---
+++ apps/sales-engine-bff/src/app/resolvers/html.resolver.ts
@@ -0,0 +1,44 @@
+import { Driver } from '@lightning/typing';
+import { Product } from '@lightning/typing';
+import { parse } from 'node-html-parser';
+
+export interface Resolver {
+    resolve(driver: Driver, data: any): Product[];
+}
+
+export default class HTMLResolver implements Resolver {
+    resolve(driver: Driver, data: any) {
+        // parse the html data
+        const html = parse(data);
+
+        const { productsDefinition } = driver;
+        const { queryPath, fields } = productsDefinition;
+
+        const items = html.querySelectorAll(queryPath);
+
+        const products: Product[] = items.map(item => {
+            const product:Product = {
+                name: '',
+                price: 0,
+                image: '',
+                link: ''
+            };
+        
+            Object.entries(fields).forEach(([key, value]) => {
+
+                // join field value with a static pre-defined value
+                // e.x. join domain + product link
+                const { fieldBase } = value;
+                let val = fieldBase || '';
+
+                val += fieldBase + value.hasOwnProperty('queryPath') ? item.querySelector(value.queryPath).getAttribute(value.propertyPath) : item.getAttribute(value.propertyPath);
+
+                product[key] = val;
+            });
+
+            return product;
+        });
+        
+        return products;
+    }
+}
