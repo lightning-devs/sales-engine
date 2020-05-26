a6f7599ea5da97ebf4267acf7b6169c1b69f245c
TELUS\x223522 <Abner.Juarez@telus.com>
2020-05-26 15:07:33 -0600

    master: code cleaning

--- apps/sales-engine-bff/src/app/resolvers/html.resolver.ts
+++ apps/sales-engine-bff/src/app/resolvers/html.resolver.ts
@@ -1,5 +1,4 @@
 import { Driver, FieldDefinition, TransformationSequence } from '@lightning/typing';
-import { Product } from '@lightning/typing';
 import { parse } from 'node-html-parser';
 import isEmpty from 'lodash/isEmpty';
 import compose from 'lodash/flow';
@@ -94,44 +93,3 @@ export default class HTMLResolver {
         return products;
     }
 }
-
-/*
-export default class HTMLResolver {
-    static resolve = (driver: Driver) => (data: any) => {
-        // parse the html data
-        const html: any = parse(data);
-
-        const { productsDefinition } = driver;
-        const { queryPath, fields } = productsDefinition;
-
-        const items = html.querySelectorAll(queryPath);
-
-        const products: Product[] = items.map(item => {
-            const product: Product = {
-                name: '',
-                price: 0,
-                image: '',
-                link: '',
-            };
-
-            Object.entries(fields).forEach(([key, value]) => {
-
-                // join field value with a static pre-defined value
-                // e.x. join domain + product link
-                const { fieldBase, getBy, transformationSequence } = value;
-
-
-                let val = fieldBase || '';
-
-                val += value.hasOwnProperty('queryPath') ? item.querySelector(value.queryPath).getAttribute(value.propertyPath) : item.getAttribute(value.propertyPath);
-
-                product[key] = val;
-            });
-
-            return product;
-        });
-
-        return products;
-    }
-}
-*/
